6ae4c046689a6595f7189bc663fd10656d93e084
Will Andrew <WA278394@falmouth.ac.uk>
2024-09-28 11:12:47 +0100

    spawner timer working

--- Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -5,11 +5,56 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField]
-    private Enemy[] enemiesToSpawn;
+    private Enemy[] enemiesArray;
+
+    [SerializeField]
+    private Enemy enemyToSpawn;
+
+    [SerializeField]
+    private float spawnCountdown;
 
     [SerializeField]
     private float spawnTimer;
 
     [SerializeField]
     private float spawnPoint;
+
+    private void Start()
+    {
+        spawnCountdown = spawnTimer;
+    }
+
+    private void Update()
+    {
+        spawnCountdown -= Time.deltaTime;
+
+        if (spawnCountdown <= 0)
+        {
+            Debug.Log("Timer at zero");
+            ResetSpawnTimer();
+        }
+    }
+
+    private void ResetSpawnTimer()
+    {
+        spawnCountdown = spawnTimer;
+    }
+
+    private void RandomiseSpawnPoint()
+    {
+
+    }
+
+    private Enemy GetEnemyToSpawn()
+    {
+        enemyToSpawn = enemiesArray[Random.Range(0, enemiesArray.Length)];
+        return enemyToSpawn;
+    }
+
+    private void SpawnEnemy()
+    {
+
+    }
+
+
 }
