8efc2fe58218725cbb0bcd4afb731dbd4600c7ec
Will Andrew <WA278394@falmouth.ac.uk>
2024-09-28 15:07:14 +0100

    enemies despawn when player out of radius

--- Assets/Scripts/EnemyScripts/EnemyParent.cs
+++ Assets/Scripts/EnemyScripts/EnemyParent.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -14,14 +15,61 @@ public class EnemyParent : MonoBehaviour
     [SerializeField]
     protected NavMeshAgent navMesh;
 
+    [SerializeField]
+    protected SphereCollider followSphere;
+
+    [SerializeField]
+    protected float destroyCountdown;
+
+    [SerializeField]
+    protected float destroyTimer;
+
+    [SerializeField]
+    protected bool isInRadius;
+
     private void Start()
     {
         navMesh = GetComponent<NavMeshAgent>();
+        followSphere = GetComponent<SphereCollider>();
+        destroyCountdown = destroyTimer;
+
+        if(followSphere.isTrigger)
+        {
+            followSphere.radius = followRadius;
+        }
     }
 
     private void Update()
     {
         navMesh.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        if(!isInRadius)
+        {
+            destroyCountdown -= Time.deltaTime;
+        }
+
+        if(destroyCountdown <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Entered Radius");
+            isInRadius = true;
+            destroyCountdown = destroyTimer;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Left radius");
+            isInRadius = false;
+        }
+    }
 }
