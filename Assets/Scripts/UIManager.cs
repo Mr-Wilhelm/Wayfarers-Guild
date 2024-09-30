a04316d49d603e139e634881ca6fc2538a08768b
JOE ORCHARD <JO278870@falmouth.ac.uk>
2024-09-30 17:20:38 +0100

    dont die

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,35 +5,45 @@ using TMPro;
 using UnityEngine.UI;
 using Unity.Netcode;
 
-public class UIManager : NetworkBehaviour
+public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] Button hostButton;
     [SerializeField] Button joinButton;
     [SerializeField] public TMP_InputField nameInputField;
-   
+
     private void Start()
     {
         //Lines for hosting and joining as a client
-        hostButton.onClick.AddListener(() => OnHost()); 
+        hostButton.onClick.AddListener(() => OnHost());
         joinButton.onClick.AddListener(() => OnJoin());
+    }
+
+    void clientDidThings()
+    {
+        Debug.Log("No server");
+
     }
 
     private void OnHost()
     {
-        Debug.Log("Hosting");
+        if (NetworkManager.Singleton.IsListening)
+        {
+            Debug.Log("Trying to host please wait...");
+            return;
+        }
         NetworkManager.Singleton.StartHost();
     }
 
     private void OnJoin()
     {
-        try
+        if (NetworkManager.Singleton.IsListening)
         {
-            NetworkManager.Singleton.StartClient();
+            Debug.Log("Trying to connect please wait...");
+            return;
         }
-        catch
-        {
-            Debug.Log("Balls");
-        }
+
+        NetworkManager.Singleton.StartClient();
     }
+
 }
