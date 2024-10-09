ec7a8ea76d14fe71228e668a396e3f28ff521389
WILL ANDREW <WA278394@falmouth.ac.uk>
2024-10-09 17:40:13 +0100

    ye no my brains dead this evening, got a grand total of very little done

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -6,13 +6,34 @@ public class PlayerInteract : MonoBehaviour
 {
     public bool canInteract;
 
+    [SerializeField]
+    private GameObject[] interactableObjects;
+
+    [SerializeField]
+    private Vector3 distanceToObject;
+
+    private void Start()
+    {
+        interactableObjects = GameObject.FindGameObjectsWithTag("Interactable");
+    }
+
     private void Update()
     {
+        //find nearest interact
+
+        foreach (var obj in interactableObjects)
+        {
+            distanceToObject = new Vector3(
+                gameObject.transform.position.x - obj.transform.position.x, 
+                gameObject.transform.position.y - obj.transform.position.y,
+                gameObject.transform.position.z - obj.transform.position.z);
+        }
+
         if(canInteract)
         {
             if(Input.GetKeyDown(KeyCode.F))
             {
-                Debug.Log("interact");
+                Debug.Log("Interact with the thing");
             }
         }
     }
