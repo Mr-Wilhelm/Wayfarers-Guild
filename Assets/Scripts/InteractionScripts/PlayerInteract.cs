1ed61f0140b26478075cb2124ee916fd03273903
Will Andrew <WA278394@falmouth.ac.uk>
2024-10-11 16:05:46 +0100

    used more while loops so preparing for a crash

--- Assets/Scripts/InteractionScripts/PlayerInteract.cs
+++ Assets/Scripts/InteractionScripts/PlayerInteract.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 
 public class PlayerInteract : MonoBehaviour
 {
+    [Header("Interaction Settings")]
     public bool canInteract;
 
     public bool canDoSpeedMinigame;
 
     public bool isInteracting;
 
+    [Header("AutoLerping Bar Settings")]
     [SerializeField]
     private float lerpingBarCurrentVal;
 
@@ -30,11 +32,24 @@ public class PlayerInteract : MonoBehaviour
     [SerializeField]
     private Image lerpingBarImage;
 
+    [Header("Player Bar Settings")]
+
+    [SerializeField]
+    private float playerBarVal;
+
+    [SerializeField]
+    private float playerBarMoveRate;
+
+    [SerializeField]
+    private Image playerBarImage;
+
     private void Start()
     {
         barMoveCountdown = 2.0f;
-        lerpRate = 0.01f;
-        
+        lerpRate = 2f;
+
+        playerBarVal = 0.5f;
+        playerBarMoveRate = 0.01f;
     }
 
     private void Update()
@@ -52,30 +67,72 @@ public class PlayerInteract : MonoBehaviour
         if(isInteracting)
         {
             DoSpeedMinigame();
+            MovePlayerBar();
         }
     }
 
     private void DoSpeedMinigame()
     {
+        MoveSpeedBar();
+    }
+
+    /// <summary>
+    /// Lerps the bar to a random value between 0 and 1
+    /// bar is clamped so that it does not go under 0 or over 1
+    /// 
+    /// </summary>
+    private void MoveSpeedBar()
+    {
         barMoveCountdown -= Time.deltaTime;
 
-        if(barMoveCountdown <= 0)
+        //reset the timer
+        if (barMoveCountdown <= 0)
         {
             lerpingBarValToReach = Random.Range(0.0f, 1.0f);
             barMoveCountdown = Random.Range(2.0f, 5.0f);
         }
 
+        //this code is probably overcomplicated, but it works so im not touching it anymore lol
+
+        //lerp down to the value
         if (lerpingBarCurrentVal > lerpingBarValToReach)
         {
-            pointInLerp -= lerpRate * Time.deltaTime;
+            pointInLerp = lerpRate* Time.deltaTime;
+
+            //clamp
+            if (pointInLerp <= 0.0f)
+            {
+                pointInLerp = 0.0f;
+            }
         }
 
+        //lerp up to the value
         else if (lerpingBarCurrentVal < lerpingBarValToReach)
         {
-            pointInLerp += lerpRate * Time.deltaTime;
+            pointInLerp = lerpRate* Time.deltaTime;
+
+            //clamp
+            if (pointInLerp >= 1.0f)
+            {
+                pointInLerp = 1.0f;
+            }
         }
 
+        //set the bar value to lerp, and move the fill amount
         lerpingBarCurrentVal = Mathf.Lerp(lerpingBarCurrentVal, lerpingBarValToReach, pointInLerp);
         lerpingBarImage.fillAmount = lerpingBarCurrentVal;
+    }
+
+    private void MovePlayerBar()
+    {
+        while(Input.GetKeyDown(KeyCode.D) && playerBarVal <= 1.0f)
+        {
+            playerBarVal += playerBarMoveRate;
+        }
+
+        while(Input.GetKeyDown(KeyCode.A) && playerBarVal >= 0.0f)
+        {
+            playerBarVal -= playerBarMoveRate;
+        }
     }
 }
