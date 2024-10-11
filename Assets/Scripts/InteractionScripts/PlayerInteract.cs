98876e2c202dd5f6476422d61cfdc2714b7334a8
WILL ANDREW <WA278394@falmouth.ac.uk>
2024-10-11 10:08:30 +0100

    bar value changes but incorrectly

--- Assets/Scripts/InteractionScripts/PlayerInteract.cs
+++ Assets/Scripts/InteractionScripts/PlayerInteract.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInteract : MonoBehaviour
 {
@@ -10,16 +11,42 @@ public class PlayerInteract : MonoBehaviour
 
     public bool isInteracting;
 
+    [SerializeField]
+    private float lerpingBarCurrentVal;
+
+    [SerializeField]
+    private float lerpingBarValToReach;
+
+    [SerializeField]
+    private float timeToLerp;
+
+    [SerializeField]
+    private Image lerpingBarImage;
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.F) && canInteract && canDoSpeedMinigame && !isInteracting)
         {
-            isInteracting = true;
+            DoSpeedMinigame();
         }
 
         else if (Input.GetKeyDown(KeyCode.F) && isInteracting)
         {
             isInteracting = false;
+        }
+    }
+
+    private void DoSpeedMinigame()
+    {
+        isInteracting = true;
+
+        if (isInteracting)
+        {
+            lerpingBarValToReach = Random.Range(0.0f, 1.0f);
+            timeToLerp = Random.Range(2.0f, 5.0f);
+
+            lerpingBarCurrentVal = Mathf.Lerp(lerpingBarCurrentVal, lerpingBarValToReach, timeToLerp * 10);
+            lerpingBarImage.fillAmount = lerpingBarCurrentVal;
         }
     }
 }
