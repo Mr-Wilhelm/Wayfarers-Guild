f0815d62aea20e385c625301772194e5227be030
JOE ORCHARD <JO278870@falmouth.ac.uk>
2024-10-17 17:29:24 +0100

    PUSH!!!!!!!!!!!!!

--- Assets/Scripts/ShipMovement/ControlWorld.cs
+++ Assets/Scripts/ShipMovement/ControlWorld.cs
@@ -48,6 +48,7 @@ public class ControlWorld : MonoBehaviour
     /// </summary>
     void ShipInputs()
     {
+        Debug.Log("cum cum beans");
         AirshipYaw = Input.GetAxisRaw("Horizontal");
         AirshipThrust = Input.GetAxisRaw("Vertical");
 
