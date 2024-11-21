001e827e2ef068282750233686db89f566ff7c36
JOE ORCHARD <JO278870@falmouth.ac.uk>
2024-11-21 16:53:42 +0000

    pogged all over the place

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,18 +7,6 @@ public class MenuManager : MonoBehaviour
 
     private bool readyPressed = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void QuestButtonPress()
     {
         Debug.Log("Quest button pressed");
@@ -27,6 +15,36 @@ public class MenuManager : MonoBehaviour
     public void BackButton()
     {
         Debug.Log("Back button pressed");
+    }
+
+    public void PortThamesButton()
+    {
+        Debug.Log("Port Thames button pressed");
+    }
+
+    public void SpoonsButton()
+    {
+        Debug.Log("Spoons button pressed");
+    }
+
+    public void FawkesRepairsButton()
+    {
+        Debug.Log("Fawkes repair button pressed");
+    }
+
+    public void ScieneceInstituteButton()
+    {
+        Debug.Log("Science Institute button pressed");
+    }
+
+    public void AirShipWrightButton()
+    {
+        Debug.Log("Air ship-wright button pressed");
+    }
+
+    public void BuckinghamPalaceButton()
+    {
+        Debug.Log("Buckingham palace button pressed");
     }
 
     public void ReadyButtonPress()
