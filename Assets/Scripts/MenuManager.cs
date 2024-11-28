5694babb19ec17cedb869c43921e842739565c38
WILL ANDREW <WA278394@falmouth.ac.uk>
2024-11-28 17:08:46 +0000

    equip and upgrade button prototype functionality

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    #region ship info
     [Header("Ship Info")]
     ShipInfo shipInfo;
+    #endregion
 
+    #region UI Parent Screens
     [Header("Ui Screens")]
 
     [SerializeField]
@@ -18,7 +23,9 @@ public class MenuManager : MonoBehaviour
 
     [SerializeField]
     private GameObject upgradesUI;
+    #endregion
 
+    #region Port Thames UI Elements
     [Header("Port Thames UI Elements")]
 
     [SerializeField]
@@ -32,7 +39,40 @@ public class MenuManager : MonoBehaviour
 
     [SerializeField]
     private GameObject portBackButton;
+    #endregion
 
+    #region Upgrade UI Elements
+    [Header("Upgrades UI Elements")]
+
+    [SerializeField]
+    private GameObject equip1Button;
+
+    [SerializeField]
+    private GameObject equip2Button;
+
+    [SerializeField]
+    private GameObject equip3Button;
+
+    [SerializeField]
+    private GameObject owned1Button;
+
+    [SerializeField]
+    private GameObject owned2Button;
+
+    [SerializeField]
+    private GameObject owned3Button;
+
+    [SerializeField]
+    private GameObject upgrades1Button;
+
+    [SerializeField]
+    private GameObject upgrades2Button;
+
+    [SerializeField]
+    private GameObject upgrades3Button;
+    #endregion
+
+    #region Constant UI Elements
     [Header("Constant UI Elements")]
 
     [SerializeField]
@@ -43,7 +83,7 @@ public class MenuManager : MonoBehaviour
 
     [SerializeField]
     private GameObject questButton;
-    
+    #endregion
 
 
     private bool readyPressed = false;
@@ -64,14 +104,26 @@ public class MenuManager : MonoBehaviour
 
         //----------Port Thames UI Elements----------
         cargoButton = GameObject.Find("CargoButton");
-
         upgradesButton = GameObject.Find("UpgradesButton");
-
         repairButton = GameObject.Find("RepairButton");
-
         portBackButton = GameObject.Find("ThamesBackButton");
         //----------Port Thames UI Elements----------
 
+        //----------Upgrades UI----------
+        equip1Button = GameObject.Find("Equip1Button");
+        equip2Button = GameObject.Find("Equip2Button");
+        equip3Button = GameObject.Find("Equip3Button");
+        owned1Button = GameObject.Find("Owned1Button");
+        owned2Button = GameObject.Find("Owned2Button");
+        owned3Button = GameObject.Find("Owned3Button");
+        upgrades1Button = GameObject.Find("Upgrades1Button");
+        upgrades2Button = GameObject.Find("Upgrades2Button");
+        upgrades3Button = GameObject.Find("Upgrades3Button");
+        //----------Upgrades UI----------
+
+        //----------City UI-----------
+
+        //----------City UI-----------
 
         //----------UI Screens---------- call this last in the Awake function, otherwise no variables are assigned
         cityUI = GameObject.Find("CityElements");
@@ -85,6 +137,8 @@ public class MenuManager : MonoBehaviour
         //----------UI Screens----------
     }
 
+    #region City UI Elements
+    //----------City UI------------
     public void QuestButtonPress()
     {
         Debug.Log("Quest button pressed");
@@ -135,9 +189,11 @@ public class MenuManager : MonoBehaviour
             readyPressed = true;
         }
     }
+    //----------City UI------------
+#endregion
 
+    #region Port Thames Functions
     //-----------Port Thames Functions------------
-
     public void PortThamesButton()
     {
         Debug.Log("Port Thames button pressed");
@@ -168,15 +224,43 @@ public class MenuManager : MonoBehaviour
     }
 
     //-----------Port Thames Functions------------
+    #endregion
 
+    #region Upgrade UI Functions
     //----------Upgrades UI Functions----------
-
     public void ReturnFromUpgrades()
     {
         Debug.Log("Returning from port Thames");
         upgradesUI.SetActive(false);
         portThamesUI.SetActive(true);
     }
-
+    public void EquipItem1()
+    {
+        Debug.Log("Item 1 Equipped");
+        Debug.Log("Items 2 and 3 unequipped");
+    }
+    public void EquipItem2()
+    {
+        Debug.Log("Item 2 Equipped");
+        Debug.Log("Items 1 and 3 unequipped");
+    }
+    public void EquipItem3()
+    {
+        Debug.Log("Item 3 Equipped");
+        Debug.Log("Items 1 and 2 unequipped");
+    }
+    public void UpgradeItem1()
+    {
+        Debug.Log("Item 1 Upgraded");
+    }
+    public void UpgradeItem2()
+    {
+        Debug.Log("Item 2 Upgraded");
+    }
+    public void UpgradeItem3()
+    {
+        Debug.Log("Item 3 Upgraded");
+    }
     //----------Upgrades UI Functions----------
+    #endregion
 }
