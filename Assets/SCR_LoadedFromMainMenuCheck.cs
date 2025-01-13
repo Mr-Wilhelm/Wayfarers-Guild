323cd12472c63ecfa29b599eb61198c91c525d1d
Joe Orchard <JO278870@falmouth.ac.uk>
2025-01-13 12:07:26 +0000

    cleaned code

--- Assets/SCR_LoadedFromMainMenuCheck.cs
+++ Assets/SCR_LoadedFromMainMenuCheck.cs
@@ -7,17 +7,16 @@ public class SCR_LoadedFromMainMenuCheck : MonoBehaviour
 {
 
     [SerializeField] private string sceneToLoad;
+
     // Start is called before the first frame update
     void Start()
     {
+        //network manager is loaded from main menu, if it does not exist you know the scene was not loaded from main menu
         if (GameObject.Find("NetworkManager") == null)
         {
+            //Loads main menu if demo scene was not loaded from main menu already
             Debug.Log("No network manager found, loading main menu");
             SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
-        }
-        else
-        {
-            Debug.Log("Network manager found, proceeding");
         }
     }
 }
