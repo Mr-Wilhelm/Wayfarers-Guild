da3bdf3824be2bc7181502c732af65f7dddafb72
Edward Hayden <eh276152@falmouth.ac.uk>
2025-01-17 22:42:51 +0000

    pain

---
+++ Assets/SCR_debugSphereLogic.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_debugSphereLogic : MonoBehaviour
+{
+    public Vector3 pos;
+    public Vector3 index;
+    public bool passable;
+}
