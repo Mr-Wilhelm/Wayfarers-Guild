acba34ec4a7906a8e2f81751356f7037ed47a868
JOE ORCHARD <JO278870@falmouth.ac.uk>
2025-01-28 13:35:00 +0000

    Added more collision code

--- Assets/SCR_ShipCollider.cs
+++ Assets/SCR_ShipCollider.cs
@@ -8,25 +8,48 @@ public class SCR_ShipCollider : MonoBehaviour
     [SerializeField] public LayerMask terrainLayer;
     [SerializeField] private float bounceForce = 10f;
     [SerializeField] private GravitasBody shipRB;
+    Vector3 desiredForward = Vector3.zero;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (((1 << other.gameObject.layer) & terrainLayer) != 0)
-        {
-            //Vector3 collisionPoint = other.ClosestPoint(transform.position); // Point of collision
-            Vector3 hitPoint = other.ClosestPoint(transform.position);
-            Vector3 bounceDirection = hitPoint.normalized;
+        ContactPoint contactPoint = collision.contacts[0];
+        Vector3 collisionPoint = contactPoint.point;
+        Debug.Log("Collision point is: " + collisionPoint);
 
-            shipRB.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
-            Debug.Log("Hit terrain at : " + hitPoint);
-        }
+        Vector3 collisionNormal = contactPoint.normal;
+
+        Bounce(collisionNormal);
+
     }
 
-    private void OnTriggerExit(Collider other)
+    private void Bounce(Vector3 colNormal)
     {
-        if (other.gameObject.layer == terrainLayer)
+        Vector3 beforeImpactVelocity = shipRB.Velocity;
+        Vector3 reflection = Vector3.Reflect(beforeImpactVelocity, colNormal);
+        Debug.Log("relection direction is: " + reflection);
+        shipRB.AddForce((reflection * 500f), ForceMode.Impulse);
+
+        // Calculate the desired direction the ship should face
+        Vector3 desiredForward = reflection.normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        if (desiredForward != Vector3.zero)
         {
-            Debug.Log("Leaving terrain");
+            Vector3 currentForward = shipRB.transform.gameObject.transform.forward; // Current direction the ship is facing
+            Vector3 rotationAxis = Vector3.Cross(currentForward, desiredForward); // Axis of rotation
+            float angle = Vector3.Angle(currentForward, desiredForward); // Angle to rotate
+
+            // If there's a significant difference, apply torque to rotate the ship
+            if (angle > 1f)  // Check if the angle is significant enough to apply torque
+            {
+                // Apply torque based on this difference (apply some scaling factor for torque strength)
+                float torqueStrength = angle * 2f; // Adjust torque strength as needed
+
+                // Apply torque to rotate around the axis
+                shipRB.AddTorque(rotationAxis * torqueStrength, ForceMode.Force);
+            }
         }
     }
 }
