d477de81259260898ce4bc707029478aa14c3b47
WILL ANDREW <WA278394@falmouth.ac.uk>
2025-02-04 10:56:58 +0000

    animation for sprite pop up

--- Assets/Code/UIScripts/New UI/SCR_NewUiManager.cs
+++ Assets/Code/UIScripts/New UI/SCR_NewUiManager.cs
@@ -5,16 +5,30 @@ using UnityEngine.UI;
 
 public class SCR_NewUiManager : MonoBehaviour
 {
+
+    [Header("Buttons")]
     [SerializeField]
     private Button spoonsButton;
+
+    [Header("Sprites")]
+    [SerializeField]
+    private GameObject spoonsSprite;
+
+    [Header("Animations")]
+    [SerializeField]
+    private Animation spoonsAnimation;
 
     private void Start()
     {
         spoonsButton = GameObject.Find("BUTTON_Spoons").GetComponent<Button>();
+
+        spoonsSprite = GameObject.Find("SPRITE_SpoonsLady");
+
+        spoonsAnimation = spoonsSprite.GetComponent<Animation>();
     }
 
     public void Func_SpoonsButtonPressed()
     {
-        Debug.Log("Spoons Button Pressed");
+        
     }
 }
