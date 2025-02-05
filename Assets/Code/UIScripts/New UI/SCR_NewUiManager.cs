2e386f3c298b01b390f7aaca803d4ede0cf1ccbe
WILL ANDREW <WA278394@falmouth.ac.uk>
2025-02-05 14:27:34 +0000

    text loading is working again

--- Assets/Code/UIScripts/New UI/SCR_NewUiManager.cs
+++ Assets/Code/UIScripts/New UI/SCR_NewUiManager.cs
@@ -9,7 +9,6 @@ using TMPro;
 
 public class SCR_NewUiManager : MonoBehaviour
 {
-
     [Header("Buttons")]
     [SerializeField]
     private Button spoonsButton;
@@ -64,7 +63,25 @@ public class SCR_NewUiManager : MonoBehaviour
         spoonsNPCDialogue = Resources.Load<TextAsset>("InkJsons/NPC1");
 
         dialoguePanel = GameObject.Find("DialogueBox");
+        dialoguePanel.SetActive(false);
+        dialogueIsPlaying = false;
+        dialogueText = dialoguePanel.GetComponentInChildren<TextMeshProUGUI>();
         //dialogue system variable assignment
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if(!dialogueIsPlaying)
+        {
+            return;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Mouse0) && dialogueIsPlaying)
+        {
+            ContinueStory();
+        }
     }
 
     public void Func_SpoonsButtonPressed()
@@ -72,7 +89,7 @@ public class SCR_NewUiManager : MonoBehaviour
         cityAnimator.SetBool("SpoonsPressed", true);
         spoonsButton.interactable = false;
 
-        //Invoke(EnterDialogueMode(spoonsNPCDialogue), 2.0f); it no likey this line my brain is exploding
+        EnterDialogueMode(spoonsNPCDialogue);
     }
 
     public void Func_SpoonsBackButtonPressed()
@@ -83,14 +100,14 @@ public class SCR_NewUiManager : MonoBehaviour
 
     #region Ink Dialogue Stuff - Tutorial used found in link Below
     //https://youtu.be/vY0Sk93YUhA
+    
     public void EnterDialogueMode(TextAsset inkJSON)
     {
-        currentStory = new Story(inkJSON.text); //sets the current story to the variable
+        currentStory = new Story(inkJSON.text);
         dialogueIsPlaying = true;
-        dialoguePanel.SetActive(true);  //activates the dialogue panel
-        audioSource.enabled = true;
+        dialoguePanel.SetActive(true);
 
-        ContinueStory();    //Loads the next line of the story
+        ContinueStory();
     }
 
     private void ExitDialogueMode()
@@ -98,89 +115,18 @@ public class SCR_NewUiManager : MonoBehaviour
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
         dialogueText.text = "";
-        audioSource.enabled = false;
     }
 
     private void ContinueStory()
     {
-        List<string> tags = currentStory.currentTags;   //gets tags from the current story
-        Debug.Log(tags.Count);
-
         if (currentStory.canContinue)
         {
-            //Stops the current text typing from playing.
-            //This fixes a bug where text overlaps from different dialogues
-            if (typeTextCoroutine != null)
-            {
-                StopCoroutine(typeTextCoroutine);
-            }
-
-            typeTextCoroutine = StartCoroutine(TypeText(currentStory.Continue())); //set text for the current line
-                                                                                   //display dialogue choices
-            DisplayChoices();   //shows button choices
+            dialogueText.text = currentStory.Continue();
         }
         else
         {
             ExitDialogueMode();
         }
-    }
-
-    /// <summary>
-    /// Takes the string, in this case the text of the current story
-    /// Convert it to an array of Chars
-    /// Iterate throug the array, adding to it
-    /// </summary>
-    /// <param name="text"></param>
-    /// <returns></returns>
-    private IEnumerator TypeText(string text)
-    {
-        dialogueText.text = "";
-        foreach (char letter in text.ToCharArray())
-        {
-            dialogueText.text += letter;
-            audioSource.clip = textSound;
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-
-            yield return new WaitForSeconds(10.0f * Time.deltaTime);
-        }
-    }
-
-    private void DisplayChoices()
-    {
-        List<Choice> currentChoices = currentStory.currentChoices;  //gets current choices from the ink file
-
-        if (currentChoices.Count > choices.Length)
-        {
-            //unity is set up to support up to three choices so far, lemme know if you want more
-            Debug.LogError("More choies were given than the Ui can support, Will made this, so ask him for help if necessary. Number of choices given:" + currentChoices.Count);
-        }
-
-        int index = 0;
-
-        //enable the choice buttons for the amount of current choices from the ink story
-        foreach (Choice choice in currentChoices)
-        {
-            choices[index].gameObject.SetActive(true);
-            choicesText[index].text = choice.text;
-            index++;
-        }
-
-
-
-        for (int i = index; i < choices.Length; i++)
-        {
-            choices[i].gameObject.SetActive(false); //go through the remaining choices the UI supports and make sure they're hidden.
-        }
-    }
-
-    public void MakeChoice(int choiceIndex)
-    {
-        //the parameter is passed on the button press in the unity editor,
-        //in the section where you assign functions to buttons
-        currentStory.ChooseChoiceIndex(choiceIndex);
     }
 
     #endregion
