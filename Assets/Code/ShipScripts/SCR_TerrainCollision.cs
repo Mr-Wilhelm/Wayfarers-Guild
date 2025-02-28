9daa7e1d1bb5b131f068e27cbe6130ff22283ee5
LOUIS PALMER <LP280500@falmouth.ac.uk>
2025-02-28 11:30:58 +0000

    fix-wip: Ship collision mostly working, shouldnt get stuck unless you really try

--- Assets/Code/ShipScripts/SCR_TerrainCollision.cs
+++ Assets/Code/ShipScripts/SCR_TerrainCollision.cs
@@ -20,35 +20,37 @@ public class SCR_TerrainCollision : MonoBehaviour
         //Debug.Log("Collision point is: " + collisionPoint);
 
         Vector3 collisionNormal = contactPoint.normal;
-
-        Bounce(collisionNormal);
+        Vector3 collisionPosition = contactPoint.point;
+        Bounce(collisionNormal*-1,collisionPosition);
         shipRecentlyHitTerrain = true;
         Invoke("ResetShipHitTerrainRecently", invincibilityPeriod);
     }
 
-    private void Bounce(Vector3 colNormal)
+    private void Bounce(Vector3 colNormal, Vector3 colPos)
     {
-        Vector3 InvertedNormal = colNormal * -1;
         Vector3 beforeImpactVelocity = shipRB.Velocity;
-        float CollisionDotProduct = Vector3.Dot(beforeImpactVelocity, InvertedNormal);
-        Debug.Log(InvertedNormal + " Normal");
+        Vector3 dis = shipRB.gameObject.transform.position - colPos;
+        float CollisionDotProduct = Vector3.Dot(beforeImpactVelocity, colNormal);
+        Debug.Log(colNormal + " Normal");
         Debug.Log(beforeImpactVelocity + " ShipDir");
         Debug.Log(CollisionDotProduct + " Result");
         
 
-        if (CollisionDotProduct < 0.25)
+
+        if (CollisionDotProduct > -0)
         {
-            shipRB.AddForce(beforeImpactVelocity.normalized *-1 * bounceForce * 10 * beforeImpactVelocity.magnitude, ForceMode.Impulse);
+            shipRB.AddForce(colNormal.normalized * bounceForce* 10, ForceMode.Impulse);
             Debug.Log("Collision DOT product mag");
         }
-        //else
-        //{
-        //    Debug.Log(beforeImpactVelocity);
-        //    Vector3 reflection = Vector3.Reflect(beforeImpactVelocity, colNormal);
-        //    Debug.Log("relection direction is: " + reflection);
-        //    shipRB.AddForce((reflection * bounceForce), ForceMode.Impulse);
-            
-        //}
+        else
+        {
+            if(beforeImpactVelocity.magnitude < 10) { beforeImpactVelocity = beforeImpactVelocity.normalized * 10; }
+            Vector3 inputDirAndRef = dis.normalized * beforeImpactVelocity.magnitude;
+            Vector3 reflection = Vector3.Reflect(inputDirAndRef*-1, colNormal);
+            Debug.Log("relection direction is: " + reflection);
+            shipRB.AddForce((reflection * bounceForce), ForceMode.Impulse);
+
+        }
     }
 
     private void ResetShipHitTerrainRecently()
