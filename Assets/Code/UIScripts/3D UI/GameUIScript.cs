f67b92f1f879605c85e09e091c6e20fab0ebbb34
WILL ANDREW <WA278394@falmouth.ac.uk>
2025-04-15 16:10:18 +0100

    its something to do with networked TextMeshProUGUI variables

--- Assets/Code/UIScripts/3D UI/GameUIScript.cs
+++ Assets/Code/UIScripts/3D UI/GameUIScript.cs
@@ -6,28 +6,22 @@ using TMPro;
 
 public class GameUIScript : NetworkBehaviour
 {
+    public QuestHandler questHandlerObject;
+
     //public NetworkVariable<TextMeshProUGUI> questPrompt = new NetworkVariable<TextMeshProUGUI>();
     //public NetworkVariable<TextMeshProUGUI> questPromptHeading = new NetworkVariable<TextMeshProUGUI>();
     //public NetworkVariable<TextMeshProUGUI> questPromptTask = new NetworkVariable<TextMeshProUGUI>();
 
-    //public QuestHandler questHandlerObject;
+    //Start is called before the first frame update
+    void Start()
+    {
+        questHandlerObject = GameObject.Find("PlayerQuestHandler").GetComponent<QuestHandler>();
 
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    questHandlerObject = GameObject.Find("PlayerQuestHandler").GetComponent<QuestHandler>();
+        //questPrompt.Value.gameObject.SetActive(true);
+        //questPromptHeading.Value.gameObject.SetActive(false);
+        //questPromptTask.Value.gameObject.SetActive(false);
 
-    //    questPrompt.Value.gameObject.SetActive(true);
-    //    questPromptHeading.Value.gameObject.SetActive(false);
-    //    questPromptTask.Value.gameObject.SetActive(false);
-
-    //    questPromptHeading.Value.text = questHandlerObject.activeQuest.Value.ToString();    
-    //    questPromptTask.Value.text = questHandlerObject.activeQuestDescription.Value.ToString();
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
+        //questPromptHeading.Value.text = questHandlerObject.activeQuest.Value.ToString();
+        //questPromptTask.Value.text = questHandlerObject.activeQuestDescription.Value.ToString();
+    }
 }
