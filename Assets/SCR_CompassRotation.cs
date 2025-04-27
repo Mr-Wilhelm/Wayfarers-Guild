bb68591d5618db7c7dd2438a7cab4d29c9cab782
Will Andrew <WA278394@falmouth.ac.uk>
2025-04-27 18:23:29 +0100

    adding the compass

---
+++ Assets/SCR_CompassRotation.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_CompassRotation : MonoBehaviour
+{
+    [SerializeField] GameObject PortalObject;
+    [SerializeField] Vector3 portalLocation;
+    [SerializeField] GameObject crystal;
+    [SerializeField] bool crystalParticlesEnabled = true;
+
+
+    void Update()
+    {
+        //All of this is in update rather than start due to networking, Start is not running as intended.
+        if (PortalObject == null)
+        {
+            PortalObject = GameObject.Find("Portal");
+            portalLocation = PortalObject.transform.position;
+        }
+        if (crystal == null)
+        {
+            crystal = this.gameObject.transform.GetChild(0).gameObject;
+        }
+        if (crystalParticlesEnabled) { crystal.transform.GetChild(1).gameObject.SetActive(true); }
+        else { crystal.transform.GetChild(1).gameObject.SetActive(false); }
+
+
+
+        //rotates the crystall to face the portal
+        if (portalLocation != null && crystal != null)
+        {
+            crystal.transform.LookAt(portalLocation);
+        }
+
+    }
+}
