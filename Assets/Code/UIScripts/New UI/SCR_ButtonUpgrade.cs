a97b97a5d949b8fa8a7002bceceabfe3c20ce3bf
WILL ANDREW <WA278394@falmouth.ac.uk>
2025-05-08 12:22:39 +0100

    started the button upgrade scripts. TODO:: Get statistics for each upgrade with the switch case statement, then get the button press to get those stats

---
+++ Assets/Code/UIScripts/New UI/SCR_ButtonUpgrade.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SCR_ButtonUpgrade : MonoBehaviour
+{
+    [SerializeField]
+    private string upgradeName;
+
+    [SerializeField]
+    private string upgradeDescription;
+
+    [SerializeField]
+    private float upgradeCost;
+
+    [SerializeField]
+    private Image upgradeImage;
+
+    private void Start()
+    {
+        //switch case assigning stats depending on name
+    }
+}
--- Assets/Code/UIScripts/New UI/SCR_Upgrades.cs
+++ Assets/Code/UIScripts/New UI/SCR_Upgrades.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+using TMPro;
 
-public class SCR_Upgrades : MonoBehaviour
+public class SCR_Upgrades : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private string descriptionString;
+
+    [SerializeField]
+    private TextMeshProUGUI descriptionToShow;
+
+    [SerializeField]
+    private TextMeshProUGUI upgradeCostToShow;
+
+    [SerializeField]
+    private Image imageToShow;
+
+    private void Start()
+    {
+        imageToShow = GameObject.Find("Icon").GetComponent<Image>();
+        descriptionToShow = GameObject.Find("UpgradeText").GetComponent<TextMeshProUGUI>();
+        upgradeCostToShow = GameObject.Find("UpgradePrice").GetComponent<TextMeshProUGUI>();
+    }
+
+    public void PressUpgradeButton()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetUpgradeInfo(string upgradeDescription, float upgradeCost, Image upgradeImage)
     {
-        
+
     }
 }
