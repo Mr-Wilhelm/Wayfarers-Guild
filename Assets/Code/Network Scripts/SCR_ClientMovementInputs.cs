30801fc2deed09e2aef473952255375147ce1ef2
LOUIS PALMER <LP280500@falmouth.ac.uk>
2025-05-09 15:21:29 +0100

    fix-wip:Player can now move and jump but needs to rotate

--- Assets/Code/Network Scripts/SCR_ClientMovementInputs.cs
+++ Assets/Code/Network Scripts/SCR_ClientMovementInputs.cs
@@ -1,3 +1,4 @@
+using Gravitas;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -6,59 +7,100 @@ using UnityEngine;
 
 public class SCR_ClientMovementInputs : NetworkBehaviour
 {
-    Vector3 TESTVEL = Vector3.forward;
+    Vector3 TESTVEL = Vector3.left;
+    float moveSpeed = 1f;
+    float jumpForce = 10f;
+    Vector2 keyInput;
+    float verticalInput;
 
-    [SerializeField] Vector3 ClientVelocity;
+
+    public NetworkVariable<Vector2> ClientInputHorizontal = new NetworkVariable<Vector2>();
+    public NetworkVariable<float> ClientInputVertical = new NetworkVariable<float>();
+
     SCR_GravBridge ServerGravBridge;
     bool foundNetworking = false;
     // Update is called once per frame
     void Update()
     {
-        if (IsOwner)
-        {
-            if (ServerGravBridge==null)
-            {
-                ServerGravBridge = this.GetComponent<SCR_GravBridge>();
-            }
-            else
-            {
-
-                //if (ClientVelocity == null) { ClientVelocity = Vector3.zero; }
-     
-                //ServerGravBridge.RecieveClientVelocity(ClientVelocity);
-            }
-        }
         if (!IsServer)
         {
-            ClientDataHandOverRpc(TESTVEL);
-            Debug.Log(TESTVEL);
+            if (IsOwner)
+            {
+
+                //Transform t = this.transform;
+
+                //// Vertical input
+                //if (Input.GetKey(KeyCode.Space))
+                //    verticalInput = 1; // Up
+                //else
+                //    verticalInput = 0; // None
+
+                keyInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+                // Vertical input
+                if (Input.GetKey(KeyCode.Space))
+                    verticalInput = 1; // Up
+                else
+                    verticalInput = 0; // None
+
+                //Vector3 velocity = Vector3.zero;
+
+                ////Left-Right movement
+                //float xForce = moveSpeed;
+                //Vector3 velocityX = keyInput.x * xForce * t.right;
+
+                //// Up-Down movement
+                //float yForce = jumpForce;
+                //Vector3 velocityY = verticalInput * yForce * t.up;
+
+                ////Checks to sort of floating point numbers issue
+                //if (velocityY.y > 0f)
+                //{
+                //    if (velocityY.x + velocityY.z <= 0.1f)
+                //    {
+                //        velocityY.x = 0f;
+                //        velocityY.z = 0f;
+                //    }
+                //}
+
+                ////Forward-Back movement
+                //float zForce = moveSpeed;
+                //Vector3 velocityZ = keyInput.y * zForce * t.forward;
+
+
+                ////Adding all velocity Vectors together
+                //velocity = velocityX + velocityY + velocityZ;
+                SetCLientVelServerRpc(keyInput);
+                SetClientInputVerticalServerRpc(verticalInput);
+            }
+
         }
-        
-
-
-        //SCR_GravBridge.replaceWithRpc(TESTVEL);
     }
 
+    private void Awake()
+    {
+        foundNetworking = true;
+    }
 
     [Rpc(SendTo.Server)]
-    void ClientDataHandOverRpc(Vector3 clientVel)
+    void SetCLientVelServerRpc(Vector3 newClientVel)
     {
-        RecieveDataHandOverServerRpc(clientVel);
+        ClientInputHorizontal.Value = newClientVel;
     }
 
-    [ServerRpc]
-    void RecieveDataHandOverServerRpc(Vector3 clientVel)
+    [Rpc(SendTo.Server)]
+    void SetClientInputVerticalServerRpc(float ClientVertInput)
     {
-        ClientVelocity = clientVel;
-        Debug.Log("Recieved on Server");
-        ServerGravBridge.RecieveClientVelocity(ClientVelocity);
+        ClientInputVertical.Value = ClientVertInput;
     }
 
 
 
 
 
-    
+
+
+
 
 
 }
--- Assets/Code/Network Scripts/SCR_GravBridge.cs
+++ Assets/Code/Network Scripts/SCR_GravBridge.cs
@@ -20,11 +20,18 @@ public class SCR_GravBridge : GravitasFirstPersonPlayerSubject
     }
     protected override void OnSubjectUpdate()
     {
-        Debug.Log(_clientVel + "PickedUP");
         if (_transform == null)
         {
             _transform = GetComponent<NetworkTransform>();
             return;
+        }
+        if (_transform.IsServer) 
+        {
+
+            SCR_ClientMovementInputs ClientInputValues = this.GetComponent<SCR_ClientMovementInputs>();
+            Debug.Log("Louis Smellz: " + ClientInputValues.ClientInputHorizontal.Value);
+            //Debug.Log(_clientVel + "PickedUP");
+        
         }
 
         //if (_transform.IsServer)  server makes the cleint rotate to the hosts inputs
@@ -62,16 +69,66 @@ public class SCR_GravBridge : GravitasFirstPersonPlayerSubject
         }
         else
         {
-            // TODO sync from client
-            return _clientVel;
+            //// TODO sync from client
+            SCR_ClientMovementInputs ClientInputValue = this.GetComponent<SCR_ClientMovementInputs>();
+            //return Scripty.ClientVelocity.Value;
+
+            Transform t = gravitasBody.CurrentTransform;
+            if (!playerOnWheel)
+            {
+                Vector3 velocity = Vector3.zero;
+
+                //Left-Right movement
+                float xForce = 10;
+                Vector3 velocityX = ClientInputValue.ClientInputHorizontal.Value.x * xForce * t.right;
+
+                // Up-Down movement
+                float yForce = 10;
+                Vector3 velocityY = ClientInputValue.ClientInputVertical.Value * yForce * t.up;
+
+                //Checks to sort of floating point numbers issue
+                if (velocityY.y > 0f)
+                {
+                    if (velocityY.x + velocityY.z <= 0.1f)
+                    {
+                        velocityY.x = 0f;
+                        velocityY.z = 0f;
+                    }
+                }
+
+                //Forward-Back movement
+                float zForce = 10;
+                Vector3 velocityZ = ClientInputValue.ClientInputHorizontal.Value.y * zForce * t.forward;
+
+
+                //Adding all velocity Vectors together
+                velocity = velocityX + velocityY + velocityZ;
+
+                //if (keyInput != Vector2.zero)
+                //{
+                //    Walking = true;
+                //    playerAnimator.SetBool("Walking", true);
+                //}
+                //else
+                //{
+                //    Walking = false;
+                //    playerAnimator.SetBool("Walking", false);
+                //}
+                return velocity;
+
+
+
+            }
+            return Vector3.zero;
+
         }
     }
 
-    //[Rpc(SendTo.Server)]
-    //void replaceWithRPCSend(Vector3 myPos) 
-    //{
+        //[Rpc(SendTo.Server)]
+        //void replaceWithRPCSend(Vector3 myPos) 
+        //{
 
-    //}
+        //}
     public void RecieveClientVelocity(Vector3 clientVel)
     {
         _clientVel = clientVel;
