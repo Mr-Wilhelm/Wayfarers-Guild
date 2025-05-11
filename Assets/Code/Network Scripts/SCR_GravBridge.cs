a31306bbace8b9265611143e228212e1ddc78bf8
Louis <lp280500@falmouth.ac.uk>
2025-05-11 17:36:50 +0100

    fix-wip: Testing for vertical rotation

--- Assets/Code/Network Scripts/SCR_GravBridge.cs
+++ Assets/Code/Network Scripts/SCR_GravBridge.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using Unity.Netcode.Components;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SCR_GravBridge : GravitasFirstPersonPlayerSubject
@@ -36,8 +37,8 @@ public class SCR_GravBridge : GravitasFirstPersonPlayerSubject
         if (_transform.IsServer) 
         {
 
-            SCR_ClientMovementInputs ClientInputValues = this.GetComponent<SCR_ClientMovementInputs>();
-            Debug.Log("Louis Smellz: " + ClientInputValues.ClientInputHorizontal.Value);
+            //SCR_ClientMovementInputs ClientInputValues = this.GetComponent<SCR_ClientMovementInputs>();
+            //Debug.Log("Louis Smellz: " + ClientInputValues.ClientInputHorizontal.Value);
             //Debug.Log(_clientVel + "PickedUP");
         
         }
@@ -52,15 +53,20 @@ public class SCR_GravBridge : GravitasFirstPersonPlayerSubject
 
             //////TODOD FINISH HERE
             SCR_ClientMovementInputs ClientInputValues = this.GetComponent<SCR_ClientMovementInputs>();
+            Vector2 keyInput = ClientInputValues.ClientInputHorizontal.Value;
+            float verticalInput = ClientInputValues.ClientInputVertical.Value;
+            Vector2 mouseInput = ClientInputValues.ClientInputMouse.Value;
 
             Transform t = gravitasBody.CurrentTransform; // Reference to either the player or the player's proxy transform
 
-            // Movement input processing
-            Vector2 keyInput = ClientInputValues.ClientInputHorizontal.Value;
-            float verticalInput = ClientInputValues.ClientInputVertical.Value;
+            Debug.Log(mouseInput+" Key 1");
 
-            // Player rotating
-            Vector2 mouseInput = ClientInputValues.ClientInputMouse.Value;
+            //Movement input processing
+            //keyInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            //Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+
+
             t.rotation *= Quaternion.AngleAxis(mouseInput.x * turnSpeed, Vector3.up);
 
             // Camera pitching
@@ -74,7 +80,7 @@ public class SCR_GravBridge : GravitasFirstPersonPlayerSubject
             {
                 angleX = Mathf.Clamp(angleX, -90, 90);
             }
-
+            
             playerCamera.transform.localRotation = Quaternion.Euler(angleX, 0, 0);
 
             // Vertical input
@@ -83,10 +89,7 @@ public class SCR_GravBridge : GravitasFirstPersonPlayerSubject
             else
                 verticalInput = 0; // None
 
-            // Interaction input
-            //if (!interact)
-            //    interact = Input.GetKeyDown(KeyCode.E);
-            // replaceWithRPCSend(base.GetInputVelocity());
+
         }
     }
 
